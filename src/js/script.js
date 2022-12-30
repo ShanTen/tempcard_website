3ec422f1104d960948262e2278fcb87d238b701f
Chanakyha <vetrichanakyha2003@gmail.com>
2022-12-31 00:45:49 +0530

    Modal is added

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,38 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
 
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open')
-  nav.classList.toggle('flex')
-  nav.classList.toggle('hidden')
-})
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
 
 let contactData = {};
 
-document.addEventListener("DOMContentLoaded", function() {
-  fields.name = document.getElementById('name');
-  fields.phoneNumber = document.getElementById('phoneNumber');
-  fields.email = document.getElementById('email');
-  fields.message = document.getElementById('message');
- })
+document.addEventListener("DOMContentLoaded", function () {
+  fields.name = document.getElementById("name");
+  fields.phoneNumber = document.getElementById("phoneNumber");
+  fields.email = document.getElementById("email");
+  fields.message = document.getElementById("message");
+});
 
-
-function contactSubmit() {
-  
-}
+function contactSubmit() {}
 
 function isNotEmpty(value) {
-  if (value == null || typeof value == 'undefined' ) return false;
-  return (value.length > 0);
- }
+  if (value == null || typeof value == "undefined") return false;
+  return value.length > 0;
+}
 
- function isNumber(num) {
-  return (num.length > 0) && !isNaN(num);
- }
+function isNumber(num) {
+  return num.length > 0 && !isNaN(num);
+}
 
- function isEmail(email) {
-  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+function isEmail(email) {
+  let regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
   return regex.test(String(email).toLowerCase());
- }  
+}
 
 function getName() {
-  return document.querySelector('input[name="gender"]:checked')
- }
+  return document.querySelector('input[name="gender"]:checked');
+}
