e62d4f54cecdd853e04bdb7685d5ca992c2b95f2
Chanakyha <vetrichanakyha2003@gmail.com>
2023-01-03 16:47:06 +0530

    Merged Contact Schema to Enquiry Schema

--- tempcard/schemas/index.ts
+++ tempcard/schemas/index.ts
@@ -1,5 +1,4 @@
 import support from './support'
 import enquiry from './enquiry'
-import contact from './contact'
 
-export const schemaTypes = [support, enquiry, contact]
+export const schemaTypes = [support, enquiry]
